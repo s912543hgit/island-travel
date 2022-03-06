c3dc5abc4d3a2995c93640b5ed2231f9c9971dc0
wang Angel <>
2022-03-06 14:37:48 +0900

    上傳原始碼

---
+++ src/views/ProductView.vue
@@ -0,0 +1,27 @@
+<template>
+  <h2>{{ product.title }}</h2>
+  <img :src="product.imageUrl" alt="">
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      product: {}
+    }
+  },
+  methods: {
+    getProduct () {
+      console.log(this.$route)
+      const { id } = this.$route.params
+      this.$http(`${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/product/${id}`)
+        .then(res => {
+          this.product = res.data.product
+        })
+    }
+  },
+  mounted () {
+    this.getProduct()
+  }
+}
+</script>
