b2faf70bbae7fe7fa57800aae627bdf79917064b
wang Angel <>
2022-03-17 21:23:10 +0900

    產品列表套版完成

--- src/views/FrontView.vue
+++ src/views/FrontView.vue
@@ -1,14 +1,17 @@
 <template>
   <FrontNavbar></FrontNavbar>
   <router-view></router-view>
+  <FooterView></FooterView>
 </template>
 
 <script>
 import FrontNavbar from '@/components/FrontNavbar.vue'
+import FooterView from '@/components/FooterView.vue'
 
 export default {
   components: {
-    FrontNavbar
+    FrontNavbar,
+    FooterView
   }
 }
 </script>
--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -1,6 +1,5 @@
 <template>
-  <h2>產品列表</h2>
-  <div class="container">
+  <!-- <div class="container">
     <div class="row row-cols-1 row-cols-lg-3 g-3">
       <div class="col" v-for="product in products" :key="product.id">
         <div class="card h-100">
@@ -13,7 +12,106 @@
         </div>
       </div>
     </div>
-  </div>
+  </div> -->
+    <div class="position-relative d-flex align-items-center justify-content-center" style="min-height: 400px;">
+      <div class="position-absolute" style="top:0; bottom: 0; left: 0; right: 0; background-image: url(https://images.unsplash.com/photo-1480399129128-2066acb5009e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80); background-position: center center; opacity: 0.1;"></div>
+      <h2 class="fw-bold">Lorem ipsum.</h2>
+    </div>
+    <div class="container mt-md-5 mt-3 mb-7">
+      <div class="row">
+        <!-- sidebar -->
+        <div class="col-md-4">
+          <div class="accordion border border-bottom border-top-0 border-start-0 border-end-0 mb-3" id="accordionExample">
+            <div class="card border-0">
+              <div class="card-header px-0 py-4 bg-white border border-bottom-0 border-top border-start-0 border-end-0 rounded-0" id="headingOne" data-bs-toggle="collapse" data-bs-target="#collapseOne">
+                <div class="d-flex justify-content-between align-items-center pe-1">
+                  <h4 class="mb-0">
+                    Lorem ipsum
+                  </h4>
+                  <i class="fas fa-chevron-down"></i>
+                </div>
+              </div>
+              <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                <div class="card-body py-0">
+                  <ul class="list-unstyled">
+                    <li><a href="#" class="py-2 d-block text-muted">Lorem ipsum</a></li>
+                    <li><a href="#" class="py-2 d-block text-muted">Lorem ipsum</a></li>
+                    <li><a href="#" class="py-2 d-block text-muted">Lorem ipsum</a></li>
+                    <li><a href="#" class="py-2 d-block text-muted">Lorem ipsum</a></li>
+                    <li><a href="#" class="py-2 d-block text-muted">Lorem ipsum</a></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div class="card border-0">
+              <div class="card-header px-0 py-4 bg-white border border-bottom-0 border-top border-start-0 border-end-0 rounded-0" id="headingThree" data-bs-toggle="collapse" data-bs-target="#collapseThree">
+                <div class="d-flex justify-content-between align-items-center pe-1">
+                  <h4 class="mb-0">
+                    Lorem ipsum
+                  </h4>
+                  <i class="fas fa-chevron-down"></i>
+                </div>
+              </div>
+              <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                <div class="card-body py-0">
+                  <ul class="list-unstyled">
+                    <li><a href="#" class="py-2 d-block text-muted">Lorem ipsum</a></li>
+                    <li><a href="#" class="py-2 d-block text-muted">Lorem ipsum</a></li>
+                    <li><a href="#" class="py-2 d-block text-muted">Lorem ipsum</a></li>
+                    <li><a href="#" class="py-2 d-block text-muted">Lorem ipsum</a></li>
+                    <li><a href="#" class="py-2 d-block text-muted">Lorem ipsum</a></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- products list -->
+        <div class="col-md-8">
+          <!-- 透過外層來決定內層的排列手法 -->
+          <div class="row row-cols-1 row-cols-md-2">
+            <div class="col" v-for="product in products" :key="product.id">
+              <!-- products card -->
+              <div class="card border-0 mb-4 position-relative position-relative">
+                <!-- <img :src="product.imageUrl" class="card-img-top rounded-0" alt="{{ product.title }}"> -->
+                <div :style="{backgroundImage: `url(${product.imageUrl})`}"
+                   style="height: 300px; background-size: cover; background-position:center "></div>
+
+                <a href="#" class="text-dark">
+                  <i class="far fa-heart position-absolute" style="right: 16px; top: 16px"></i>
+                </a>
+                <div class="card-body p-0">
+                  <h4 class="mb-0 mt-3">
+                    {{ product.title }}
+                  </h4>
+                  <p class="card-text mb-0">NT{{ product.origin_price }} <span class="text-muted "><del>NT{{ product.price }}</del></span></p>
+                  <p class="text-muted mt-3">{{ product.description }}</p>
+                  <router-link :to="`/product/${product.id}`" class="btn btn-primary">查看產品</router-link>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- pagination -->
+          <nav class="d-flex justify-content-center">
+            <ul class="pagination">
+              <li class="page-item">
+                <a class="page-link" href="#" aria-label="Previous">
+                  <span aria-hidden="true">&laquo;</span>
+                </a>
+              </li>
+              <li class="page-item active"><a class="page-link" href="#">1</a></li>
+              <li class="page-item"><a class="page-link" href="#">2</a></li>
+              <li class="page-item"><a class="page-link" href="#">3</a></li>
+              <li class="page-item">
+                <a class="page-link" href="#" aria-label="Next">
+                  <span aria-hidden="true">&raquo;</span>
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </div>
 </template>
 
 <script>
@@ -25,7 +123,7 @@ export default {
   },
   methods: {
     getProducts () {
-      this.$http(`${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/products/all`)
+      this.$http(`${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/products`)
         .then(res => {
           this.products = res.data.products
         })
